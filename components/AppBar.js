e888230353a4c1de5f6e5076a588fa7d38333a2b
Santiago <santo1996.29@gmail.com>
2022-08-04 16:48:00 -0400

    setup workspace settings

---
+++ components/AppBar.js
@@ -0,0 +1,17 @@
+import { Box } from 'grommet'
+
+function AppBar(props) {
+  return (
+    <Box
+      tag="header"
+      direction="row"
+      align="center"
+      justify="between"
+      background="brand"
+      pad={{ left: 'medium', right: 'small', vertical: 'small' }}
+      elevation="medium"
+      style={{ zIndex: '1' }}
+      {...props}
+    />
+  )
+}
